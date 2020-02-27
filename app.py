e6aa21ac09a7f572ea407035430a8dd3d572cf84
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-02-26 23:48:56 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -20,4 +20,3 @@ def doawnload():
     return send_from_directory('', 'proxies.csv', as_attachment=True)
 if __name__ == "__main__":  # if we are running the app from command line(__main__)
     app.run(debug=True, threaded = True)  # it will turn on debug mode
-
